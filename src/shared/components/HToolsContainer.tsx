f28d10c03b97ea24af3e555c9661c969bf7db66b
Sebastian Aguirre <osebas15@gmail.com>
2025-01-18 21:33:38 -0500

    tools container

---
+++ src/shared/components/HToolsContainer.tsx
@@ -0,0 +1,34 @@
+import { 
+    Component,
+    For,
+    JSX
+} from "solid-js";
+
+import styles from "./HToolsContainer.module.css"
+
+interface ToolContainer {
+    title: string
+    tool: () => JSX.Element
+    color: string
+}
+
+interface HToolsContainer {
+    containers: [ToolContainer]
+}
+
+const HToolsContainer: Component<HToolsContainer> = ({containers}) => {
+    return (
+        <div class={styles.h_tools_container}>
+            <For each={containers} fallback={<a>error</a>}>
+            { (toolContainer) => 
+                <div>
+                    {toolContainer.title}
+                    {toolContainer.tool()}                
+                </div>
+            }
+            </For>
+        </div>
+    )
+}
+
+export default HToolsContainer
