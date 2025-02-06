50ab28b1f6b43e4add4ae7ab3af5897e1a403503
Sebastian Aguirre <osebas15@gmail.com>
2025-02-05 20:46:45 -0500

    started sharelist menu

--- src/shared/components/sharelist/ShareList.tsx
+++ src/shared/components/sharelist/ShareList.tsx
@@ -1,11 +1,11 @@
 import { 
-    Component, 
-    createEffect, 
-    For, 
-    createSignal, 
-    createResource,
-    onMount,
-    onCleanup
+  Component, 
+  createEffect, 
+  For, 
+  createSignal, 
+  createResource,
+  onMount,
+  onCleanup
 } from 'solid-js';
 
 import {
@@ -26,17 +26,17 @@ interface ShareListProps {
 }
 
 const createLoadTodos = (list_id?: string) => (async () => {
-    const { data, error } = await supabase
-        .from<Todo>('todos')
-        .select()
-        .eq('list_id', list_id)
+  const { data, error } = await supabase
+    .from<Todo>('todos')
+    .select()
+    .eq('list_id', list_id)
 
-    if (error) {
-        console.error(error)
-        throw error
-    }
+  if (error) {
+    console.error(error)
+    throw error
+  }
 
-    return data
+  return data
 })
 
 const orderTodos = (todos: Todo[]): Todo[] => {
@@ -63,146 +63,167 @@ const orderTodos = (todos: Todo[]): Todo[] => {
 }
 
 const ShareList: Component<ShareListProps> = ({list_id}) => {
+  let [upstreamTodos, {mutate, refetch}] = createResource(createLoadTodos(list_id))
+  let [todos, setTodos] = createStore<Todo[]>([])
+  let [inputTodo, setInputTodo] = createSignal<string>('')
+  let [shareUrl, setShareUrl] = createSignal<string>('')
+  let [menuOpen, setMenuOpen] = createSignal<boolean>(false);
 
-    let [upstreamTodos, {mutate, refetch}] = createResource(createLoadTodos(list_id))
-    let [todos, setTodos] = createStore<Todo[]>([])
-    let [inputTodo, setInputTodo] = createSignal<string>('')
-    let [shareUrl, setShareUrl] = createSignal<string>('')
+  let subscription: RealtimeSubscription | null
 
-    let subscription: RealtimeSubscription | null
+  let location = useLocation()
+  
+  createEffect(() => {
+    const newShareUrl = `${window.location.origin}${location.pathname}`
+    if (newShareUrl) {
+      setShareUrl(newShareUrl)
+    }
 
-    let location = useLocation()
-    
-    createEffect(() => {
-      const newShareUrl = `${window.location.origin}${location.pathname}`
-      if (newShareUrl) {
-        setShareUrl(newShareUrl)
+    const newTodos = upstreamTodos()
+    if (newTodos) {
+      setTodos(newTodos)
+    }
+  })
+
+  const createSubscription = () => {
+    subscription = supabase
+    .from<Todo>(`todos:list_id=eq.${list_id}`)
+    .on('*', (payload) => {
+      switch (payload.eventType) {
+        case 'INSERT':
+          setTodos((prev) => [...prev, payload.new])
+          break
+        case 'UPDATE':
+          setTodos((item) => item.id === payload.new.id, payload.new)
+          break
+        case 'DELETE':
+          setTodos((prev) => prev.filter((item) => item.id != payload.old.id))
+          break
       }
+    })
+    .subscribe()
+  }
 
-      const newTodos = upstreamTodos()
-      if (newTodos) {
-        setTodos(newTodos)
+  const handleVisibilityChange = () => {
+    if (!document.hidden) {
+        createSubscription();
+    }
+  };
+
+  onMount(() => {
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
+        createTodo({
+            task: inputTodo(),
+            is_complete: false,
+            list_id: list_id ?? 'error'
+        })
       }
     })
 
-    const createSubscription = () => {
-      subscription = supabase
-      .from<Todo>(`todos:list_id=eq.${list_id}`)
-      .on('*', (payload) => {
-        switch (payload.eventType) {
-          case 'INSERT':
-            setTodos((prev) => [...prev, payload.new])
-            break
-          case 'UPDATE':
-            setTodos((item) => item.id === payload.new.id, payload.new)
-            break
-          case 'DELETE':
-            setTodos((prev) => prev.filter((item) => item.id != payload.old.id))
-            break
-        }
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+    createSubscription()
+  })
+
+  onCleanup(() => {
+    document.removeEventListener('visibilitychange', handleVisibilityChange);
+    if (subscription) {
+        supabase.removeSubscription(subscription);
+    }
+  });
+
+  async function completeTodo(id: number) {
+    const { error } = await supabase
+      .from<Todo>('todos')
+      .update({
+        is_complete: true
       })
-      .subscribe()
+      .eq('id', id)
+    if (error) {
+      console.error(error)
     }
+  }
 
-    const handleVisibilityChange = () => {
-      if (!document.hidden) {
-          createSubscription();
-      }
-    };
-
-    onMount(() => {
-      document.addEventListener('keydown', (e) => {
-        if (e.key === 'Enter') {
-          createTodo({
-              task: inputTodo(),
-              is_complete: false,
-              list_id: list_id ?? 'error'
-          })
-        }
-      })
-
-      document.addEventListener('visibilitychange', handleVisibilityChange);
-
-      createSubscription()
-    })
-
-    onCleanup(() => {
-      document.removeEventListener('visibilitychange', handleVisibilityChange);
-      if (subscription) {
-          supabase.removeSubscription(subscription);
-      }
-    });
-
-    async function completeTodo(id: number) {
-        const { error } = await supabase
-          .from<Todo>('todos')
-          .update({
-            is_complete: true
-          })
-          .eq('id', id)
-        if (error) {
-          console.error(error)
-        }
+  async function createTodo(newTodo: Todo){
+    const { data, error } = await supabase.from<Todo>('todos').insert(newTodo)
+    if (error) {
+      console.error(error)
     }
+    setInputTodo('')
+  }
 
-    async function createTodo(newTodo: Todo){
-      const { data, error } = await supabase.from<Todo>('todos').insert(newTodo)
-      if (error) {
-        console.error(error)
-      }
-      setInputTodo('')
+  async function deleteTodo(todo: Todo) {
+    const { error } = await supabase
+      .from<Todo>('todos')
+      .delete()
+      .eq('id', todo.id)
+    if (error) {
+      console.error(error)
     }
+  }
 
-    async function deleteTodo(todo: Todo) {
-      const { error } = await supabase
-        .from<Todo>('todos')
-        .delete()
-        .eq('id', todo.id)
-      if (error) {
-        console.error(error)
-      }
+  async function todoActions(verb: TodoVerb, updatedTodos: Todo[]){
+    switch (verb) {
+        case 'COMPLETE':
+            updatedTodos.forEach((todo) => completeTodo(todo.id!))
+            break
+        case 'CREATE':
+            updatedTodos.forEach((todo) => createTodo(todo))
+            break
+        case 'DELETE':
+            updatedTodos.forEach((todo) => deleteTodo(todo))
+        default :
+            console.error(`unimplemented switch case ${verb}`)
     }
+  }
 
-    async function todoActions(verb: TodoVerb, updatedTodos: Todo[]){
-        switch (verb) {
-            case 'COMPLETE':
-                updatedTodos.forEach((todo) => completeTodo(todo.id!))
-                break
-            case 'CREATE':
-                updatedTodos.forEach((todo) => createTodo(todo))
-                break
-            case 'DELETE':
-                updatedTodos.forEach((todo) => deleteTodo(todo))
-            default :
-                console.error(`unimplemented switch case ${verb}`)
-        }
-    }
+  async function newList() {
 
-    return (
-        <div class={styles.main}>
-          <b class={contStyles.title}>Share your Todo List</b>
-          <a>Share this link to share your list!</a>
-          <CopyToClipboard text={`${window.location.origin}${location.pathname}`}/> 
-          <div class={styles.inputContainer}>
-            <input
-                type="text"
-                name="todo"
-                value={inputTodo()}
-                onInput={(e) => setInputTodo(e.target.value)}
-            />
-            <button onClick={() => todoActions('CREATE', [{
-                task: inputTodo(),
-                is_complete: false,
-                list_id: list_id ?? 'error'
-            }])}>
-                New todo
-            </button>
-          </div>
-          <For each={orderTodos(todos)}>
-              {(todo) => <TodoCell todo={todo} action={todoActions}/>}
-          </For>
+  }
+
+  return (
+    <div class={styles.main}>
+      <div class={styles.header}>
+        <b class={contStyles.title}>Share your Todo List</b>
+        <div class={styles.burgerIcon} onClick={() => setMenuOpen(!menuOpen())}>
+          &#9776;
         </div>
-    )
+      </div>
+      <div class={styles.overlayMenuContainer}>
+      {menuOpen() && (
+        <div class={styles.overlayMenu}>
+          <ul>
+            <li onClick={() => console.log("1")}></li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+          </ul>
+        </div>
+      )}
+      </div>
+      <a>Share this link to share your list!</a>
+      <CopyToClipboard text={`${window.location.origin}${location.pathname}`}/> 
+      <div class={styles.inputContainer}>
+        <input
+          type="text"
+          name="todo"
+          value={inputTodo()}
+          onInput={(e) => setInputTodo(e.target.value)}
+        />
+        <button onClick={() => todoActions('CREATE', [{
+          task: inputTodo(),
+          is_complete: false,
+          list_id: list_id ?? 'error'
+        }])}>
+          New todo
+        </button>
+      </div>
+      <For each={orderTodos(todos)}>
+        {(todo) => <TodoCell todo={todo} action={todoActions}/>}
+      </For>
+    </div>
+  )
 };
 
 export default ShareList
